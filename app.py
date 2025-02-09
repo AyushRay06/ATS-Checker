ea3f91ba4644c373b4b73a3612e951b36b2f0e10
AyushRay06 <rayayush47@gmail.com>
2025-02-10 01:55:59 +0530

    gemini

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ description:{jd}
 I want the response in one single string having the structure
 {{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
 """
-
 ## streamlit app
 st.title("Smart ATS")
 st.text("Improve Your Resume ATS")
